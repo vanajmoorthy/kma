ec16d31859ab4039548846f46141f816166be889
Vanaj Moorthy <vanajmoorthy@gmail.com>
2023-06-26 10:57:10 +0530

    Fade in page on load

--- pages/index.vue
+++ pages/index.vue
@@ -1,69 +1,71 @@
 <template>
-	<div class="container" :class="{ 'is-loaded': loaded }">
-		<div id="title-holder">
-			<div id="spacer">g</div>
-			<div id="control">
-				<h1>KM&A</h1>
-				<img class="down-arrow" src="@/assets/down.png" alt="down arrow" @click="scrollToNextSection" />
+	<transition name="fade">
+		<div class="container" v-if="loaded">
+			<div id="title-holder">
+				<div id="spacer">g</div>
+				<div id="control">
+					<h1>KM&A</h1>
+					<img class="down-arrow" src="@/assets/down.png" alt="down arrow" @click="scrollToNextSection" />
+				</div>
+				<div id="project-name">
+					<transition name="fade">
+						<h3 key="projectName">{{ currentProject }} <img class="right-arrow" src="@/assets/forward.png"
+								alt="View project" /> </h3>
+					</transition>
+				</div>
 			</div>
-			<div id="project-name">
-				<transition name="fade">
-					<h3 key="projectName">{{ currentProject }} <img class="right-arrow" src="@/assets/forward.png"
-							alt="View project" /> </h3>
-				</transition>
-			</div>
-		</div>
-		<div class="project1" ref="section1"></div>
-		<div class="about" ref="section2">
-			<div>
-				<p>
-					Established in 1990, Kumar Moorthy & Associates is an award
-					winning partnership firm of architects and interior
-					designers based in Delhi. In the twenty nine years since
-					inception we have undertaken and successfully completed a
-					wide range of design works in the diverse fields of
-					residential design and interiors, institutional and hotel
-					design, commercial and office spaces and interiors. The
-					clients for the above include notable industrial groups,
-					private limited companies, charitable and non government
-					organisations, government agencies and public figures other
-					than private individuals.
+			<div class="project1" ref="section1"></div>
+			<div class="about" ref="section2">
+				<div>
+					<p>
+						Established in 1990, Kumar Moorthy & Associates is an award
+						winning partnership firm of architects and interior
+						designers based in Delhi. In the twenty nine years since
+						inception we have undertaken and successfully completed a
+						wide range of design works in the diverse fields of
+						residential design and interiors, institutional and hotel
+						design, commercial and office spaces and interiors. The
+						clients for the above include notable industrial groups,
+						private limited companies, charitable and non government
+						organisations, government agencies and public figures other
+						than private individuals.
 
-					<br />
-					<br />
-					The firm has been honoured to receive awards in various
-					categories under the prestigious J.K. Architect of the Year
-					Awards in 1995 and 1997, and the Indian Institute of
-					Interior Designers- MK Award 2004 & 2006, Best Office
-					Architecture India, Asia Pacific Property Awards
-					Architecture 2018, The I.I.A National Awards for Excellence
-					2018.
-				</p>
-				<p>
-					We have executed projects in the diverse locations of Delhi
-					N.C.R including Noida , Gurgaon, Manesar & Bawal as also in
-					Agra, Mumbai, Indore (M.P.) and Kullu (H.P).
-					<br /><br />
-					Recent notable projects by the firm span a diverse spectrum
-					of concept, philosophy and materials and include an building
-					in the Indo Saracenic heritage campus of a 125 year old
-					public school in Indore and a low cost building using &
-					exemplifying alternative technology for a rehabilitation
-					centre in West Delhi for an NGO for disabled persons to a
-					factory for a large upcoming Indo American lighting JV in
-					Bawal, Haryana other than corporate office buildings &
-					interiors in Gurgaon & NOIDA and a small number of
-					significant residential projects including farmhouses.
-				</p>
+						<br />
+						<br />
+						The firm has been honoured to receive awards in various
+						categories under the prestigious J.K. Architect of the Year
+						Awards in 1995 and 1997, and the Indian Institute of
+						Interior Designers- MK Award 2004 & 2006, Best Office
+						Architecture India, Asia Pacific Property Awards
+						Architecture 2018, The I.I.A National Awards for Excellence
+						2018.
+					</p>
+					<p>
+						We have executed projects in the diverse locations of Delhi
+						N.C.R including Noida , Gurgaon, Manesar & Bawal as also in
+						Agra, Mumbai, Indore (M.P.) and Kullu (H.P).
+						<br /><br />
+						Recent notable projects by the firm span a diverse spectrum
+						of concept, philosophy and materials and include an building
+						in the Indo Saracenic heritage campus of a 125 year old
+						public school in Indore and a low cost building using &
+						exemplifying alternative technology for a rehabilitation
+						centre in West Delhi for an NGO for disabled persons to a
+						factory for a large upcoming Indo American lighting JV in
+						Bawal, Haryana other than corporate office buildings &
+						interiors in Gurgaon & NOIDA and a small number of
+						significant residential projects including farmhouses.
+					</p>
+				</div>
 			</div>
-		</div>
-		<div class="project2" ref="section3"></div>
-		<div class="project3" ref="section4"></div>
-		<div class="project4" ref="section5"></div>
-		<div id="spacer2"></div>
+			<div class="project2" ref="section3"></div>
+			<div class="project3" ref="section4"></div>
+			<div class="project4" ref="section5"></div>
+			<div id="spacer2"></div>
 
-		<Footer ref="section6" id="footer"></Footer>
-	</div>
+			<Footer ref="section6" id="footer"></Footer>
+		</div>
+	</transition>
 </template>
 
 <script>
